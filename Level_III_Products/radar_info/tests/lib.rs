0264eb32074fa82fdbc73aeb336625f1c0d22a0d
Jonathan Wesley Stone <js@piratejon.com>
2015-08-02 22:53:23 -0500

    green

--- Level_III_Products/radar_info/tests/lib.rs
+++ Level_III_Products/radar_info/tests/lib.rs
@@ -3,9 +3,8 @@ extern crate radar_info;
 
 #[test]
 fn ri_read_a_file() {
-//  let mut radar_parser : radar_info::RadarFileParser = std::default::Defaultl::default();
+  let mut radar_parser : radar_info::RadarFileParser = std::default::Default::default();
 
-//  radar_parser.init("sn.last");
-  assert!(false);
+  assert!(radar_parser.load_file("sn.last"));
 }
 
