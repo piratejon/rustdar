92a6e7091fad56a05913e95480238edc61614360
Jonathan Wesley Stone <js@piratejon.com>
2015-08-02 22:24:34 -0500

    add some items to get started on parsing radar files

---
+++ Level_III_Products/radar_info/tests/lib.rs
@@ -0,0 +1,11 @@
+
+extern crate radar_info;
+
+#[test]
+fn ri_read_a_file() {
+//  let mut radar_parser : radar_info::RadarFileParser = std::default::Defaultl::default();
+
+//  radar_parser.init("sn.last");
+  assert!(false);
+}
+
